1df36480b7e9d080ee55a542367f48ce6c182408
III2D <30813936+III2D@users.noreply.github.com>
2017-08-18 00:21:26 +0100

    Basic Engine Work

---
+++ Third Person Survival/Assets/Engine/Scripts/Misc/Splash.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Splash : MonoBehaviour {
+
+    public Text SplashText;
+
+    void Start ()
+    {
+        StartCoroutine(i_Splash());
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator i_Splash()
+    {
+        yield return new WaitForSeconds(1);
+        SplashText.text = "<size=25>THIS IS A</size><size=50> DEVELOPMENT BUILD </size><size=25>EXPECT BUGS AND MISSING FEATURES</size>".ToString();
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("scn_Development");
+    }
+}
